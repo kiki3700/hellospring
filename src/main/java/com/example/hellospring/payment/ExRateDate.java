084dfbbeb82f5e67616fc96284e1fdf3b2759b19
kiki3700 <5575492q@gmail.com>
2024-07-23 14:07:45 +0000

    first commit - before refactor

---
+++ src/main/java/com/example/hellospring/payment/ExRateDate.java
@@ -0,0 +1,10 @@
+package com.example.hellospring.payment;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateDate(String result, Map<String, BigDecimal> rates) {
+}
