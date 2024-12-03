c71e71a4b48f7b65726256f668e82bdb6382bbe7
kiki3700 <5575492q@gmail.com>
2024-12-03 12:05:57 +0000

    jdbc repository

---
+++ src/main/java/com/example/hellospring/payment/data/JdbcOrderRepository.java
@@ -0,0 +1,12 @@
+package com.example.hellospring.payment.data;
+
+/**
+ * Project: hello-spring <br>
+ * JdbcOrderRepository <br>
+ *
+ * @author sunghyunlee <br>
+ * Date: 2024/12/03 <br>
+ * Time: <br>
+ */
+public class JdbcOrderRepository {
+}
